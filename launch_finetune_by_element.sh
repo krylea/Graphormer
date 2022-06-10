9e9665f49844b78c5cf22afa335fb3fcee8971c2
Kira Selby <kaselby@uwaterloo.ca>
2022-06-10 01:32:59 -0400

    finetune

--- launch_finetune_by_element.sh
+++ launch_finetune_by_element.sh
@@ -2,11 +2,12 @@
 
 ELEMENTS=(17 37 55 75 81)
 SPLITS=(50 200 1000 5000)
+model="subatom"
 
 for e in "${ELEMENTS[@]}"
 do
     for s in "${SPLITS[@]}"
     do
-        sbatch finetune_oc20_by_element.sh "finetune_$e_$s" $e $s
+        sbatch finetune_oc20_by_element.sh "finetune_${e}_${s}_${model}" $e $s
     done
 done
