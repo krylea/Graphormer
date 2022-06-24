556b852758a04a4b27f4ad786dda85f0bbb50d74
Kira Selby <kaselby@uwaterloo.ca>
2022-06-24 15:13:06 -0400

    subshell

--- graphormer/models/subatom_modules.py
+++ graphormer/models/subatom_modules.py
@@ -25,6 +25,7 @@ class FixedAtomEmbedding(nn.Module):
 
 class SubshellEmbedding(nn.Module):
     def __init__(self, embed_dim, n_max, l_max, atom_configs, occupancy_correction=False):
+        super().__init__()
         self.embed_dim = embed_dim
         self.atom_configs = atom_configs    # Nele x nmax*lmax
         self.subshell_embeds = nn.Parameter(torch.empty(n_max * l_max, embed_dim))
@@ -59,6 +60,7 @@ class SubshellValenceEmbedding(nn.Module):
         return cls(embed_dim, hidden_dim, n_max, l_max, valence_configs, core_configs, occupancy_correction)
 
     def __init__(self, embed_dim, hidden_dim, n_max, l_max, valence_configs, core_configs, occupancy_correction=False):
+        super().__init__()
         self.valence_embeds = SubshellEmbedding(embed_dim, n_max, l_max, valence_configs, occupancy_correction)
         self.core_embeds = SubshellEmbedding(embed_dim, n_max, l_max, core_configs, occupancy_correction)
         #self.merge = nn.Sequential(
