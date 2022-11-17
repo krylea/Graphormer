a0ac2b7cfb145ba152b7bd3a199b520c8fb65eef
Kira Selby <kaselby@uwaterloo.ca>
2022-11-17 14:31:51 -0500

    kfold

---
+++ launch_kfold.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+MODELS=("base" "paw" "properties" "subshell")
+dataset=$1
+folds=8
+
+for (( i=0; i<$folds; i++ ))
+do
+    for model in "${MODELS[@]}"
+    do
+        sbatch finetune_carol.sh "kfold_${dataset}/${model}_${i}" $model "${dataset}/${i}"
+    done
+done
