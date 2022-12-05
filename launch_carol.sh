0be3b504ca62de913fb39e90e3cc766ec093171a
Kira Selby <kaselby@uwaterloo.ca>
2022-12-05 11:24:10 -0500

    less gpus

--- launch_carol.sh
+++ launch_carol.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=logs/slurm-%j.txt
 #SBATCH --open-mode=append
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:2
 #SBATCH --partition=t4v1,t4v2,p100,rtx6000
 #SBATCH --cpus-per-gpu=1
 #SBATCH --mem=25GB
