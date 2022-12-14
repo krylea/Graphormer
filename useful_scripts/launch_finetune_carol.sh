b44af95eb384bf45093d0d7abb1c764de3d612ba
Kira Selby <kaselby@uwaterloo.ca>
2022-12-14 16:51:33 -0500

    add atoms

--- useful_scripts/launch_finetune_carol.sh
+++ useful_scripts/launch_finetune_carol.sh
@@ -2,10 +2,11 @@
 
 MODELS=("base" "paw" "properties" "subshell")
 dataset=$1
+prefix=$2
 
 for model in "${MODELS[@]}"
 do
-    sbatch finetune_carol.sh "finetune_carol_${dataset}_${model}" $model $dataset
+    sbatch useful_scripts/finetune_carol.sh "${prefix}_${dataset}_${model}" $model $dataset
 done
 
-sbatch train_carol.sh "finetune_carol_${dataset}_nofinetune" $dataset
+sbatch useful_scripts/train_carol.sh "${prefix}_${dataset}_nofinetune" $dataset
