ce2d7672acc2461d2d6c2edb76784366259595db
Kira Selby <kaselby@uwaterloo.ca>
2022-12-16 11:23:43 -0500

    finish

---
+++ useful_scripts/finish_kfold.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+MODELS=("base" "paw" "properties" "subshell")
+dataset=$1
+prefix=$2
+folds=8
+
+
+for (( i=0; i<$folds; i++ ))
+do
+    for model in "${MODELS[@]}"
+    do
+        folder = "$prefix_${dataset}/${model}_${i}"
+        if [ ! -a "${folder}/checkpoint_last.pt" ]
+        then
+            sbatch finetune_carol.sh $folder $model "${dataset}/kfold/${i}"
+        fi
+    done
+
+    #sbatch launch_carol.sh "$prefix_${dataset}/nofinetune_${i}" "${dataset}/kfold/${i}"
+done
